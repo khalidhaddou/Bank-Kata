c4fd7822a7cba53eb2418315b4430c4033cb4cf4
khalidhaddou <khalidhaddou123456@gmail.com>
2025-01-30 19:48:12 +0100

    Mise à jour du README et modifications du projet

--- bank-kata/src/Account.java
+++ bank-kata/src/Account.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Collections;
 public class Account implements AccountService {
     final List<Transaction> transactions = new ArrayList<>();
 
@@ -20,7 +20,11 @@ public class Account implements AccountService {
         int balance = 0;
         for (Transaction transaction : transactions) {
             balance += transaction.getAmount();
+        }
+        for (int i = transactions.size() - 1; i >= 0; i--) {
+            Transaction transaction = transactions.get(i);
             System.out.println(transaction.getDate() + " | " + transaction.getAmount() + " | " + balance);
+            balance -= transaction.getAmount();
         }
     }
 }
--- bank-kata/src/AccountTest.java
+++ bank-kata/src/AccountTest.java
@@ -13,5 +13,17 @@ class AccountTest {
         assertEquals(3, ((Account) account).transactions.size());  // Devrait contenir 3 transactions
     }
 
+    @Test
+    public void testPrintStatement() {
+        Account account = new Account();
+        account.deposit(1000);
+        account.deposit(2000);
+        account.withdraw(500);
+
+        // Rediriger la sortie et tester l'affichage
+        assertDoesNotThrow(() -> account.printStatement());
+    }
+
+
 }
 
